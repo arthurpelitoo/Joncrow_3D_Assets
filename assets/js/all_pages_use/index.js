d1c67cc560f06d499b4ab28a4c50c66176149a84
arthurpelitoo <arthur.pelito@escola.pr.gov.br>
2025-05-26 23:56:27 -0300

    fiz reestruturacao de arquivos para a possibilidade de criar features de maneira escalavel e organizada

--- assets/js/all_pages_use/index.js
+++ assets/js/all_pages_use/index.js
@@ -10,7 +10,7 @@
 
 const button = document.getElementById("header-menu");
 const navegacao = document.getElementById("header-nav");
-// const header = document.getElementById("header");
+// const header = document.getElementById("header"); // comentei para guardar depois essa ideia para outro projeto. (deixar header invisivel no topo e depois de scrollar fica com alguma cor de fundo.)
 
 console.log(button, navegacao, header);
 
@@ -23,7 +23,7 @@ function toggleEvent(){
     }
 }
 
-// function headerChange(){
+// function headerChange(){ // comentei para guardar depois essa ideia para outro projeto.
 
 //     if(window.scrollY > 50 && window.innerWidth > 1000){
 //         header.classList.add('scrolled');
@@ -35,4 +35,4 @@ function toggleEvent(){
 
 button.addEventListener("click", toggleEvent);
 
-// window.addEventListener("scroll", headerChange);
+// window.addEventListener("scroll", headerChange); // comentei para guardar depois essa ideia para outro projeto.
